9a050f2d16d1d514498711b8abff50dc701b4aa3
luiz200208 <00001123580492sp@al.educacao.sp.gov.br>
2024-11-19 14:00:27 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'historia',
+    'O que foi a guerra fria ?',
+    'A Guerra Fria foi um período marcado por um conflito político-ideológico travado entre Estados Unidos e a ex-União Soviética (URSS), entre 1947 e 1991. Esse período polarizou o mundo em dois grandes blocos, um alinhado ao capitalismo e outro alinhado ao comunismo.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o maior deserto do mundo?',
+    'O continente Antártida (ou Antártica) é o maior deserto do mundo, com mais de 14 milhões de quilômetros quadrados de extensão,'
+)
+
+criaCartao(
+    'matematica',
+    'para que serve a matematica?',
+    'Matemática é a ciência que relaciona saberes abstratos e concretos. Entre outras coisas, ela estuda os números, as formas, as grandezas e as possibilidades. A Matemática é um conjunto de diferentes formas de pensar e entender o mundo.'
+)
+
+criaCartao(
+    'Lingua portuguesa',
+    'qual foi o primeiro idioma usado no Brasil?',
+    'o tupi (mais precisamente, o tupinambá, uma língua do litoral brasileiro da família tupi-guarani) foi usado como língua geral na colônia, ao lado do portuguêss'
+)
